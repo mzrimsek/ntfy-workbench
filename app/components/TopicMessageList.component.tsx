cf1c74d8f3f46547df6bbf101394966f7d936335
Mike Zrimsek <mikezrimsek@gmail.com>
2024-04-01 01:59:53 -0400

    display description

--- app/components/TopicMessageList.component.tsx
+++ app/components/TopicMessageList.component.tsx
@@ -13,6 +13,9 @@ const TopicMessageList: React.FC<TopicMessageListProps> = ({
   return (
     <div>
       {topicConfig && <h2>{topicConfig.name}</h2>}
+      {topicConfig && topicConfig.description && (
+        <p>{topicConfig.description}</p>
+      )}
       <ul>
         {messages.map((message, index) => (
           <li key={index}>{JSON.stringify(message)}</li>
