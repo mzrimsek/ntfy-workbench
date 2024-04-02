b14aab3d535e5e60bdc0dc313409d2f123a9aaa4
Mike Zrimsek <mikezrimsek@gmail.com>
2024-04-02 02:18:55 -0400

    give it a color for light mode

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -98,7 +98,7 @@ export default function Index() {
 
   return (
     <div>
-      <header className="sticky top-0 z-50 flex items-center justify-between px-4 py-4 shadow-md dark:bg-slate-700">
+      <header className="sticky top-0 z-50 flex items-center justify-between px-4 py-4 shadow-md dark:bg-slate-700 bg-white">
         <h1 className="text-xl font-bold">Ntfy Workbench</h1>
         <DisplayStateSwitch
           displayState={displayState}
